dfbe1a37530095edaade7380ba18b29e495fbf2b
vinayakagarwal12 <vinayak.agarwal@iiitb.org>
2022-04-17 18:58:51 +0530

    test cases added

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,41 +2,41 @@ package calculator;
 import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-public class Calculator {
+public class Calculator
+{
     private static final Logger logger = LogManager.getLogger(Calculator.class);
 
     public static double square_root(double x)
     {
         double ans = Math.sqrt(x);
-        System.out.println("Square root of "+x+" = "+ans);
+        logger.info("Square root of "+x+" = "+ans);
+        System.out.println("Ans = "+ans+"\n");
+        System.out.println("");
         return ans;
     }
     public static int factorial(int num){
+        if(num<0)
+            return 0;
         int res = 1 ;
         for(int i=num; i>=1; i--) {
             res = res*i;
         }
-        logger.info("Factorial of "+num);
-        logger.info("!"+num+" = "+res);
-        System.out.println("The Result is "+res+"\n");
+        logger.info("Factorial of "+num+" = "+res);
+        System.out.println("Ans = "+res+"\n");
         System.out.println("");
         return res;
     }
     public static double natural_log(double num){
         double res = Math.log(num);
-        logger.info("Natural logarithm of"+num);
-        logger.info("log("+num+") = "+res);
-//        System.out.println("");
-        System.out.println("The Result is "+ res+"\n");
+        logger.info("Natural logarithm of "+num+" = "+res);
+        System.out.println("Ans = "+res+"\n");
         System.out.println("");
         return res;
     }
     public static double power_func(double num, double exp){
         double res = Math.pow(num,exp);
-        logger.info(num+" powers "+exp);
-        logger.info(num+"^"+exp+" = "+res);
-//        System.out.println("");
-        System.out.println("The Result is "+ res+"\n");
+        logger.info(num+" powers "+exp+" = "+res);
+        System.out.println("Ans = "+res+"\n");
         System.out.println("");
         return res;
     }
@@ -44,48 +44,48 @@ public class Calculator {
     {
         Scanner reader = new Scanner(System.in);
         int flag=0;
-        int op;
+        int operation;
         int num_int;
         double num_double,num_x,b;
         while(flag==0)
         {
             System.out.println("Enter the operation number \n"+
-                    "1.Square root \n"+
+                    "1.Square Root \n"+
                     "2.Factorial \n"+
-                    "3.Natural logarithm (base е) \n"+
-                    "4.Power function\n"+
+                    "3.Natural Logarithm (base е) \n"+
+                    "4.Power Function\n"+
                     "5:Exit\n");
-            op = reader.nextInt();
-            if(op==5)
+            operation = reader.nextInt();
+            if(operation==5)
             {
                 flag=1;
             }
-            else {
-                switch(op) {
-                    case 1:
-                        System.out.print("Enter number: ");
-                        num_double = reader.nextDouble();
-                        square_root(num_double);
-                        break;
-                    case 2:
-                        System.out.print("Enter number: ");
-                        num_int = reader.nextInt();
-                        factorial(num_int);
-                        break;
-                    case 3:
-                        System.out.print("Enter number: ");
-                        num_double = reader.nextDouble();
-                        natural_log(num_double);
-                        break;
-                    case 4:
-                        System.out.print("Enter number: ");
-                        num_x = reader.nextDouble();
-                        System.out.print("exponent: ");
-                        b=reader.nextDouble();
-                        power_func(num_x,b);
-                        break;
-                    default:
-                        System.out.println("Invalid Code Entered try again");
+            else
+            {
+                if(operation==1) {
+                    System.out.print("Enter number: ");
+                    num_double = reader.nextDouble();
+                    square_root(num_double);
+                }
+                else if(operation==2) {
+                    System.out.print("Enter number: ");
+                    num_int = reader.nextInt();
+                    factorial(num_int);
+                }
+                else if(operation==3) {
+                    System.out.print("Enter number: ");
+                    num_double = reader.nextDouble();
+                    natural_log(num_double);
+                }
+                else if(operation==4) {
+                    System.out.print("Enter number: ");
+                    num_x = reader.nextDouble();
+                    System.out.print("Enter exponent: ");
+                    b=reader.nextDouble();
+                    power_func(num_x,b);
+                }
+                else {
+                    System.out.println("Invalid Number Entered try again");
                 }
             }
         }
